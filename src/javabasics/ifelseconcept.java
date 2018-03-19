a176fce0e0e0ef1dcee2b5c50f7b6124827809c8
praveen <praveen@DESKTOP-DVJCPPH>
2018-03-19 22:19:17 +0530

    if else concept

---
+++ src/javabasics/ifelseconcept.java
@@ -0,0 +1,21 @@
+package javabasics;
+
+public class ifelseconcept {
+
+	public static void main(String[] args) {
+	int a1 = 30;
+	int b1 = 20;
+	if(b1>a1){
+		System.out.println("b1 greater than a1");
+	}
+		else {
+			System.out.println("a1 greater than b1");
+			
+		}
+	
+	}
+	
+
+	}
+
+
